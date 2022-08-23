bef823b6844a0939af224759612b2b88645be3c5
saadiakhan1999 <57935155+saadiakhan1999@users.noreply.github.com>
2022-08-24 00:05:18 +0400

    Add files via upload

---
+++ user.js
@@ -0,0 +1,9 @@
+
+
+var user = [
+    ["Ali", "ali@gmail.com", "03227570252"],
+    ["Ahmad", "Ahmad@gmail.com", "03227570285"],
+    ["Sadia", "Sadia@gmail.com", "03227570289"],
+    ["Gazifa", "Gazifa@gmail.com", "032275702936"],
+];
+
